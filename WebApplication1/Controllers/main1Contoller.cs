484d36ee7811489eba1d2ae71d69d49568de569c
tamanghawana0107 <hawanatamang@gmail.com>
2022-03-11 09:21:34 +0545

    Second Commit

---
+++ WebApplication1/Controllers/main1Contoller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class main1Contoller : Controller
+    {
+        mainEntities db = new mainEntities();
+        // GET: main1
+        public ActionResult student()
+        {
+            List<student> data = db.students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/studentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class studentController : Controller
+    {
+        mainEntities db = new mainEntities();
+        // GET: Main
+        public ActionResult student()
+        {
+            List<student> data = db.students.ToList();
+            return View(data);
+        }
+    }
+}
