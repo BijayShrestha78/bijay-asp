b8cb119e22879d66890343fb6e3f7cfec4a02088
tamanghawana0107 <hawanatamang@gmail.com>
2022-03-11 08:51:04 +0545

    changed

--- WebApplication1/Controllers/MainController.cs
+++ WebApplication1/Controllers/MainController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class MainController : Controller
     {
+        mainEntities db=new mainEntities(); 
         // GET: Main
         public ActionResult mainview()
         {
-            return View();
+            List<employee> data = db.employees.ToList();
+            return View(data);
         }
     }
 }
--- WebApplication1/Controllers/asp bijayy.cs
+++ WebApplication1/Controllers/asp bijayy.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class MainController : Controller
+    public class aspbijay : Controller
     {
+        mainEntities db= new mainEntities();
+
         // GET: Main
         public ActionResult mainview()
         {
+            List<employee> data = db.employees.ToList();
             return View();
         }
     }
